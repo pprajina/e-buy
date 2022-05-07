d183b11982bdf97d3a07c147b7e969d1d097d011
pprajina <105094409+pprajina@users.noreply.github.com>
2022-05-07 15:34:46 +0545

    Update dummyData.js

--- admin/src/dummyData.js
+++ admin/src/dummyData.js
@@ -1,251 +1,93 @@
-export const userData = [
-    {
-      name: "Jan",
-      "Active User": 4000,
-    },
-    {
-      name: "Feb",
-      "Active User": 3000,
-    },
-    {
-      name: "Mar",
-      "Active User": 5000,
-    },
-    {
-      name: "Apr",
-      "Active User": 4000,
-    },
-    {
-      name: "May",
-      "Active User": 3000,
-    },
-    {
-      name: "Jun",
-      "Active User": 2000,
-    },
-    {
-      name: "Jul",
-      "Active User": 4000,
-    },
-    {
-      name: "Aug",
-      "Active User": 3000,
-    },
-    {
-      name: "Sep",
-      "Active User": 4000,
-    },
-    {
-      name: "Oct",
-      "Active User": 1000,
-    },
-    {
-      name: "Nov",
-      "Active User": 4000,
-    },
-    {
-      name: "Dec",
-      "Active User": 3000,
-    },
-  ];
-
-  export const productData = [
-    {
-      name: "Jan",
-      "Sales": 4000,
-    },
-    {
-      name: "Feb",
-      "Sales": 3000,
-    },
-    {
-      name: "Mar",
-      "Sales": 5000,
-    },
-  ];
-
   export const userRows = [
     {
       id: 1,
-      username: "Jon Snow",
+      username: "New User 1",
       avatar:
-        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "jon@gmail.com",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScdHD-KQQkMQdJcXTSALcRVFp7chjRbA0e-w&usqp=CAU",
+      email: "newuser@gmail.com",
       status: "active",
       transaction: "$120.00",
     },
     {
       id: 2,
-      username: "Jon Snow",
+      username: "New User 2",
       avatar:
-        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "jon@gmail.com",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScdHD-KQQkMQdJcXTSALcRVFp7chjRbA0e-w&usqp=CAU",
+      email: "newuser@gmail.com",
       status: "active",
       transaction: "$120.00",
     },
     {
       id: 3,
-      username: "Jon Snow",
+      username: "New User 3",
       avatar:
-        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "jon@gmail.com",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScdHD-KQQkMQdJcXTSALcRVFp7chjRbA0e-w&usqp=CAU",
+      email: "newuser@gmail.com",
       status: "active",
       transaction: "$120.00",
     },
     {
       id: 4,
-      username: "Jon Snow",
+      username: "New User 4",
       avatar:
-        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "jon@gmail.com",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScdHD-KQQkMQdJcXTSALcRVFp7chjRbA0e-w&usqp=CAU",
+      email: "newuser@gmail.com",
       status: "active",
       transaction: "$120.00",
     },
     {
       id: 5,
-      username: "Jon Snow",
+      username: "New User 5",
       avatar:
-        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "jon@gmail.com",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScdHD-KQQkMQdJcXTSALcRVFp7chjRbA0e-w&usqp=CAU",
+      email: "newuser@gmail.com",
       status: "active",
       transaction: "$120.00",
     },
     {
       id: 6,
-      username: "Jon Snow",
+      username: "New User 6",
       avatar:
-        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "jon@gmail.com",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScdHD-KQQkMQdJcXTSALcRVFp7chjRbA0e-w&usqp=CAU",
+      email: "newuser@gmail.com",
       status: "active",
       transaction: "$120.00",
     },
     {
       id: 7,
-      username: "Jon Snow",
+      username: "New User 7",
       avatar:
-        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "jon@gmail.com",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScdHD-KQQkMQdJcXTSALcRVFp7chjRbA0e-w&usqp=CAU",
+      email: "newuser@gmail.com",
       status: "active",
       transaction: "$120.00",
     },
     {
       id: 8,
-      username: "Jon Snow",
+      username: "New User 8",
       avatar:
-        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "jon@gmail.com",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScdHD-KQQkMQdJcXTSALcRVFp7chjRbA0e-w&usqp=CAU",
+      email: "newuser@gmail.com",
       status: "active",
       transaction: "$120.00",
     },
     {
       id: 9,
-      username: "Jon Snow",
+      username: "New User 9",
       avatar:
-        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "jon@gmail.com",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScdHD-KQQkMQdJcXTSALcRVFp7chjRbA0e-w&usqp=CAU",
+      email: "newuser@gmail.com",
       status: "active",
       transaction: "$120.00",
     },
     {
       id: 10,
-      username: "Jon Snow",
+      username: "New User 10",
       avatar:
-        "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "jon@gmail.com",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScdHD-KQQkMQdJcXTSALcRVFp7chjRbA0e-w&usqp=CAU",
+      email: "newuser@gmail.com",
       status: "active",
       transaction: "$120.00",
     },
   ];
 
-  export const productRows = [
-    {
-      id: 1,
-      name: "Apple Airpods",
-      img:
-        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      stock: 123,
-      status: "active",
-      price: "$120.00",
-    },
-    {
-      id: 2,
-      name: "Apple Airpods",
-      img:
-        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      stock: 123,
-      status: "active",
-      price: "$120.00",
-    },
-    {
-      id: 3,
-      name: "Apple Airpods",
-      img:
-        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      stock: 123,
-      status: "active",
-      price: "$120.00",
-    },
-    {
-      id: 4,
-      name: "Apple Airpods",
-      img:
-        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      stock: 123,
-      status: "active",
-      price: "$120.00",
-    },
-    {
-      id: 5,
-      name: "Apple Airpods",
-      img:
-        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      stock: 123,
-      status: "active",
-      price: "$120.00",
-    },
-    {
-      id: 6,
-      name: "Apple Airpods",
-      img:
-        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      stock: 123,
-      status: "active",
-      price: "$120.00",
-    },
-    {
-      id: 7,
-      name: "Apple Airpods",
-      img:
-        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      stock: 123,
-      status: "active",
-      price: "$120.00",
-    },
-    {
-      id: 8,
-      name: "Apple Airpods",
-      img:
-        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      stock: 123,
-      status: "active",
-      price: "$120.00",
-    },
-    {
-      id: 9,
-      name: "Apple Airpods",
-      img:
-        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      stock: 123,
-      status: "active",
-      price: "$120.00",
-    },
-    {
-      id: 10,
-      name: "Apple Airpods",
-      img:
-        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      stock: 123,
-      status: "active",
-      price: "$120.00",
-    },
-  ];
